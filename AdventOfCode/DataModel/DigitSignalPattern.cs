213e9f26ca830fb76fc176b4ac0f2b6176821649
Victor VERHILLE <victor.verhille@diginext.fr>
2021-12-08 17:24:02 +0100

    clean day 8

--- AdventOfCode/DataModel/DigitSignalPattern.cs
+++ AdventOfCode/DataModel/DigitSignalPattern.cs
@@ -28,6 +28,20 @@ namespace AdventOfCode.DataModel
     /// </summary>
     public class DigitSignalPattern
     {
+        #region Fields
+
+        /// <summary>
+        /// Stores the coded digits linked to the amount of segments.
+        /// </summary>
+        private Dictionary<int, List<Segments>> mCodedSegmentsByAmountOfSegments = new Dictionary<int, List<Segments>>();
+
+        /// <summary>
+        /// Stores the int by decoded segments.
+        /// </summary>
+        private Dictionary<int, Segments> mDecodedIntBySegments = new Dictionary<int, Segments>();
+
+        #endregion Fields
+
         #region Properties
 
         /// <summary>
@@ -48,10 +62,6 @@ namespace AdventOfCode.DataModel
             private set;
         }
 
-        private Dictionary<int, List<string>> mNumberOfSegmentsByCodedLines = new Dictionary<int, List<string>>();
-
-        private Dictionary<int, Segments> mDecodedIntBySegments = new Dictionary<int, Segments>();
-
         #endregion Properties
 
         #region Constructors
@@ -63,6 +73,7 @@ namespace AdventOfCode.DataModel
         public DigitSignalPattern(string pLine)
         {
             this.InitializesData(pLine);
+            this.Decode();
         }
 
         #endregion Constructors
@@ -81,12 +92,11 @@ namespace AdventOfCode.DataModel
             this.SignalPatterns.AddRange(lSplit.GetRange(0, 10));
             this.Output.AddRange(lSplit.GetRange(10, 4));
 
-            this.mNumberOfSegmentsByCodedLines = new Dictionary<int, List<string>>();
+            this.mCodedSegmentsByAmountOfSegments = new Dictionary<int, List<Segments>>();
             for (int lIndex = 0; lIndex <= 7; lIndex++)
             {
-                this.mNumberOfSegmentsByCodedLines.Add(lIndex, this.SignalPatterns.Where(pElem => pElem.Count() == lIndex).ToList());
+                this.mCodedSegmentsByAmountOfSegments.Add(lIndex, this.SignalPatterns.Where(pElem => pElem.Count() == lIndex).Select(pString => this.StringToSegment(pString)).ToList());
             }
-            this.Decode();
         }
 
         /// <summary>
@@ -98,57 +108,97 @@ namespace AdventOfCode.DataModel
             return this.Output.Aggregate(0, (pAcc, pNext) => pAcc += (pNext.Count() == 2 || pNext.Count() == 3 || pNext.Count() == 4 || pNext.Count() == 7) ? 1 : 0 , pAcc => pAcc);
         }
 
+        /// <summary>
+        /// Computes the value 1.
+        /// </summary>
         private void ComputeDigit1()
         {
-            this.mDecodedIntBySegments.Add(1, this.StringToSegment(this.mNumberOfSegmentsByCodedLines[2].First()));
+            this.mDecodedIntBySegments.Add(1, this.mCodedSegmentsByAmountOfSegments[2].First());
         }
+
+        /// <summary>
+        /// Computes the value 7.
+        /// </summary>
         private void ComputeDigit7()
         {
-            this.mDecodedIntBySegments.Add(7, this.StringToSegment(this.mNumberOfSegmentsByCodedLines[3].First()));
+            this.mDecodedIntBySegments.Add(7, this.mCodedSegmentsByAmountOfSegments[3].First());
         }
+
+        /// <summary>
+        /// Computes the value 4.
+        /// </summary>
         private void ComputeDigit4()
         {
-            this.mDecodedIntBySegments.Add(4, this.StringToSegment(this.mNumberOfSegmentsByCodedLines[4].First()));
+            this.mDecodedIntBySegments.Add(4, this.mCodedSegmentsByAmountOfSegments[4].First());
         }
+
+        /// <summary>
+        /// Computes the value 8.
+        /// </summary>
         private void ComputeDigit8()
         {
             this.mDecodedIntBySegments.Add(8, Segments.All);
         }
+
+        /// <summary>
+        /// Computes the value 3.
+        /// </summary>
         private void ComputeDigit3()
         {
-            IEnumerable<Segments> lSegments = this.mNumberOfSegmentsByCodedLines[5].Select(pString => this.StringToSegment(pString));
-            this.mDecodedIntBySegments.Add(3, lSegments.First(pValue => pValue.HasFlag(this.mDecodedIntBySegments[1])));
+            this.mDecodedIntBySegments.Add(3, this.mCodedSegmentsByAmountOfSegments[5].First(pValue => pValue.HasFlag(this.mDecodedIntBySegments[1])));
         }
+
+        /// <summary>
+        /// Computes the value 6.
+        /// </summary>
         private void ComputeDigit6()
         {
-            this.mDecodedIntBySegments.Add(6, this.mNumberOfSegmentsByCodedLines[6].Select(pString => this.StringToSegment(pString)).First(pValue => pValue.HasFlag(this.mDecodedIntBySegments[1]) == false));
+            this.mDecodedIntBySegments.Add(6, this.mCodedSegmentsByAmountOfSegments[6].First(pValue => pValue.HasFlag(this.mDecodedIntBySegments[1]) == false));
         }
+
+        /// <summary>
+        /// Computes the value 9.
+        /// </summary>
         private void ComputeDigit9()
         {
             Segments l47 = this.mDecodedIntBySegments[4] | this.mDecodedIntBySegments[7];
-            Segments l235 = this.mNumberOfSegmentsByCodedLines[5].Aggregate(Segments.All, (pAcc, pNext) => pAcc &= this.StringToSegment(pNext), pAcc => pAcc);
+            Segments l235 = this.mCodedSegmentsByAmountOfSegments[5].Aggregate(Segments.All, (pAcc, pNext) => pAcc &= pNext, pAcc => pAcc);
             this.mDecodedIntBySegments.Add(9, l47 | l235);
         }
+
+        /// <summary>
+        /// Computes the value 0.
+        /// </summary>
         private void ComputeDigit0()
         {
-            List<Segments> lSegments = this.mNumberOfSegmentsByCodedLines[6].Select(pString => this.StringToSegment(pString)).ToList();
+            List<Segments> lSegments = this.mCodedSegmentsByAmountOfSegments[6].ToList();
             lSegments.Remove(this.mDecodedIntBySegments[6]);
             lSegments.Remove(this.mDecodedIntBySegments[9]);
             this.mDecodedIntBySegments.Add(0, lSegments.First());
         }
+
+        /// <summary>
+        /// Computes the value 5.
+        /// </summary>
         private void ComputeDigit5()
         {
-            List<Segments> lSegments = this.mNumberOfSegmentsByCodedLines[5].Select(pString => this.StringToSegment(pString)).ToList();
-            this.mDecodedIntBySegments.Add(5, lSegments.First(pSeg => (pSeg | this.mDecodedIntBySegments[1]) == this.mDecodedIntBySegments[9]));
+            this.mDecodedIntBySegments.Add(5, this.mCodedSegmentsByAmountOfSegments[5].First(pSeg => (pSeg | this.mDecodedIntBySegments[1]) == this.mDecodedIntBySegments[9]));
         }
+
+        /// <summary>
+        /// Computes the value 2.
+        /// </summary>
         private void ComputeDigit2()
         {
-            List<Segments> lSegments = this.mNumberOfSegmentsByCodedLines[5].Select(pString => this.StringToSegment(pString)).ToList();
+            List<Segments> lSegments = this.mCodedSegmentsByAmountOfSegments[5].ToList();
             lSegments.Remove(this.mDecodedIntBySegments[3]);
             lSegments.Remove(this.mDecodedIntBySegments[5]);
             this.mDecodedIntBySegments.Add(2, lSegments.First());
         }
 
+        /// <summary>
+        /// Decodes the digits according to the signal pattern.
+        /// </summary>
         private void Decode()
         {
             this.ComputeDigit1();
@@ -163,6 +213,11 @@ namespace AdventOfCode.DataModel
             this.ComputeDigit2();
         }
 
+        /// <summary>
+        /// Gets the value from a coded segment.
+        /// </summary>
+        /// <param name="pSegments"></param>
+        /// <returns></returns>
         private int FromCodedDigitGetValue(Segments pSegments)
         {
             int lResult = -1;
@@ -177,12 +232,20 @@ namespace AdventOfCode.DataModel
             return lResult;
         }
 
+        /// <summary>
+        /// Decode the output as a int.
+        /// </summary>
+        /// <returns></returns>
         public int GetDecodedOutput()
         {
             return this.Output.Aggregate(0, (pResult, pDigit) => pResult = 10 * pResult + this.FromCodedDigitGetValue(this.StringToSegment(pDigit)), pResult => pResult);
         }
 
-
+        /// <summary>
+        /// Transforms a string into a segment.
+        /// </summary>
+        /// <param name="pString"></param>
+        /// <returns></returns>
         private Segments StringToSegment(string pString)
         {
             Segments lResult = Segments.None;
