f0ac2492717273ad2cc2c8769939ba70d36c139e
Victor VERHILLE <victor.verhille@diginext.fr>
2021-12-17 13:13:10 +0100

    solve day 17

---
+++ AdventOfCode/DataModel/Shoot.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.DataModel
+{
+    /// <summary>
+    /// Defines a shoot.
+    /// </summary>
+    public class Shoot
+    {
+        #region Fields
+
+        /// <summary>
+        /// Stores a flag indicating whether the shoot has reached 0.
+        /// </summary>
+        private bool mHasReached0;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Initial X speed
+        /// </summary>
+        public int InitialXSpeed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initial Y speed
+        /// </summary>
+        public int InitialYSpeed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Current X Speed.
+        /// </summary>
+        public int CurrentXSpeed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Current Y SPeed.
+        /// </summary>
+        public int CurrentYSpeed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Current X pos.
+        /// </summary>
+        public int CurrentX
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Current Y pos.
+        /// </summary>
+        public int CurrentY
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the max y it ever reached.
+        /// </summary>
+        public int MaxY
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the Y speed when the probe is under 0 for the first time.
+        /// </summary>
+        public int YSpeedWhenFirstTimeUnder0
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Shoot"/> class.
+        /// </summary>
+        /// <param name="pInitialXSpeed"></param>
+        /// <param name="pInitialYSpeed"></param>
+        public Shoot(int pInitialXSpeed, int pInitialYSpeed)
+        {
+            this.InitialXSpeed = pInitialXSpeed;
+            this.InitialYSpeed = pInitialYSpeed;
+            this.CurrentX = 0;
+            this.CurrentY = 0;
+            this.MaxY = this.CurrentY;
+            this.CurrentXSpeed = this.InitialXSpeed;
+            this.CurrentYSpeed = this.InitialYSpeed;
+            this.mHasReached0 = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Step.
+        /// </summary>
+        private void Step()
+        {
+            this.CurrentX += this.CurrentXSpeed;
+            this.CurrentY += this.CurrentYSpeed;
+
+            if (this.CurrentXSpeed > 0)
+            {
+                this.CurrentXSpeed--;
+            }
+            else if (this.CurrentXSpeed < 0)
+            {
+                this.CurrentXSpeed++;
+            }
+
+            this.CurrentYSpeed--;
+
+            if (!this.mHasReached0)
+            {
+                if (this.CurrentY <= 0)
+                {
+                    this.mHasReached0 = true;
+                    this.YSpeedWhenFirstTimeUnder0 = this.CurrentYSpeed;
+                }
+            }
+
+            if (this.CurrentYSpeed == 0)
+            {
+                this.MaxY = this.CurrentY;
+            }
+        }
+
+        /// <summary>
+        /// Runs towards a target area.
+        /// </summary>
+        /// <param name="pTargetArea"></param>
+        public void Run(TargetArea pTargetArea)
+        {
+            bool lIsInOrOut = false;
+            while (!lIsInOrOut)
+            {
+                lIsInOrOut = this.IsInsideArea(pTargetArea) || this.ShouldStop(pTargetArea);
+                if (!lIsInOrOut)
+                {
+                    this.Step();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if we should stop, as it will never reach the target area.
+        /// </summary>
+        /// <param name="pShoot"></param>
+        /// <returns></returns>
+        private bool ShouldStop(TargetArea pTargetArea)
+        {
+            return pTargetArea.MaxX < this.CurrentX || pTargetArea.MinY > this.CurrentY || (pTargetArea.MinX > this.CurrentX && this.CurrentXSpeed == 0);
+        }
+
+        /// <summary>
+        /// Returns true if a shoot is inside the target area.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInsideArea(TargetArea pTargetArea)
+        {
+            return pTargetArea.MinX <= this.CurrentX && pTargetArea.MaxX >= this.CurrentX && pTargetArea.MinY <= this.CurrentY && pTargetArea.MaxY >= this.CurrentY;
+        }
+
+        /// <summary>
+        /// To string.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("X: {0} |Y: {1}", this.InitialXSpeed, this.InitialYSpeed);
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Defines the target area.
+    /// </summary>
+    public struct TargetArea
+    {
+        /// <summary>
+        /// Initializes a new instance of the target area.
+        /// </summary>
+        /// <param name="pMinX"></param>
+        /// <param name="pMaxX"></param>
+        /// <param name="pMinY"></param>
+        /// <param name="pMaxY"></param>
+        public TargetArea(int pMinX, int pMaxX, int pMinY, int pMaxY)
+        {
+            this.MinX = pMinX;
+            this.MaxX = pMaxX;
+            this.MinY = pMinY;
+            this.MaxY = pMaxY;
+        }
+
+        /// <summary>
+        /// Min X
+        /// </summary>
+        public int MinX
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Max X
+        /// </summary>
+        public int MaxX
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Min Y
+        /// </summary>
+        public int MinY
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Max Y
+        /// </summary>
+        public int MaxY
+        {
+            get;
+        }
+    }
+}
--- AdventOfCode/Days/Day17.cs
+++ AdventOfCode/Days/Day17.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,21 @@ namespace AdventOfCode.Days
     {
         #region Fields
 
+        /// <summary>
+        /// Stores the target area.
+        /// </summary>
+        private TargetArea mTargetArea;
+
+        /// <summary>
+        /// Stores the shoots that goes inside the area.
+        /// </summary>
+        private List<Shoot> mGoodShoots = new List<Shoot>();
+
+        /// <summary>
+        /// Stores the maxY.
+        /// </summary>
+        private int mMaxY;
+
         #endregion Fields
 
         #region Properties
@@ -24,7 +40,7 @@ namespace AdventOfCode.Days
         {
             get
             {
-                return DataProvider.DAY17TESTINPUTS_PATH;
+                return DataProvider.DAY17INPUTS_PATH;
             }
         }
 
@@ -35,7 +51,7 @@ namespace AdventOfCode.Days
         {
             get
             {
-                return pInput => pInput.First();
+                return pInput => this.ComputePart1(pInput);
             }
         }
 
@@ -46,7 +62,7 @@ namespace AdventOfCode.Days
         {
             get
             {
-                return pInput => pInput.First();
+                return pInput => this.ComputePart2(pInput);
             }
         }
 
@@ -71,6 +87,113 @@ namespace AdventOfCode.Days
         /// <param name="pInput"></param>
         private void InitializeData(IEnumerable<string> pInput)
         {
+            string lLine = pInput.First();
+            lLine = lLine.Remove(0, 12);
+            string[] lSplit = lLine.Split(',');
+            string [] lXSplit = lSplit[0].Remove(0, 3).Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            string [] lYSplit = lSplit[1].Remove(0, 3).Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            this.mTargetArea = new TargetArea(int.Parse(lXSplit[0]), int.Parse(lXSplit[1]), int.Parse(lYSplit[0]), int.Parse(lYSplit[1]));
+        }
+
+        /// <summary>
+        /// Computes the part 1.
+        /// </summary>
+        /// <param name="pInput"></param>
+        /// <returns></returns>
+        private string ComputePart1(IEnumerable<string> pInput)
+        {
+            this.InitializeData(pInput);
+            this.RunPart1();
+            this.mMaxY = this.mGoodShoots.Aggregate(0, (pAcc, pNext) => pAcc = Math.Max(pAcc, pNext.InitialYSpeed), pAcc => pAcc);
+            return this.MaxHeightFromGoodShots().ToString();
+        }
+
+        /// <summary>
+        /// Computes the part 1.
+        /// </summary>
+        /// <param name="pInput"></param>
+        /// <returns></returns>
+        private string ComputePart2(IEnumerable<string> pInput)
+        {
+            this.mGoodShoots.Clear();
+            this.RunPart2();
+            return this.mGoodShoots.Count().ToString();
+        }
+
+        /// <summary>
+        /// Returns the max height from good shots.
+        /// </summary>
+        /// <returns></returns>
+        private int MaxHeightFromGoodShots()
+        {
+            return this.mGoodShoots.Aggregate(0, (pAcc, pNext) => pAcc = Math.Max(pAcc, pNext.MaxY), pAcc => pAcc);
+        }
+
+        /// <summary>
+        /// Runs part 1.
+        /// </summary>
+        private void RunPart1()
+        {
+            int lInitialXSpeed = 1;
+            int lInitialYSpeed = 1;
+            bool lMaxYReached = false;
+            while (!lMaxYReached)
+            {
+                bool lXTooFar = false;
+                lInitialXSpeed = 1;
+                while (!lXTooFar)
+                {
+                    Shoot lShoot = new Shoot(lInitialXSpeed, lInitialYSpeed);
+                    lShoot.Run(this.mTargetArea);
+                    if (lShoot.IsInsideArea(this.mTargetArea))
+                    {
+                        this.mGoodShoots.Add(lShoot);
+                    }
+                    lInitialXSpeed++;
+                    lMaxYReached = this.IsTooFarOnY(lShoot);
+                    lXTooFar = this.IsTooFarOnX(lShoot) || lMaxYReached;
+                }
+                lInitialYSpeed++;
+            }
+        }
+
+        /// <summary>
+        /// Runs part 2.
+        /// </summary>
+        private void RunPart2()
+        {
+            for (int lX = 0; lX < this.mTargetArea.MaxX + 1; lX++)
+            {
+                for (int lY = this.mMaxY + 1; lY > this.mTargetArea.MinY - 1; lY--)
+                {
+                    Shoot lShoot = new Shoot(lX, lY);
+                    lShoot.Run(this.mTargetArea);
+                    if (lShoot.IsInsideArea(this.mTargetArea))
+                    {
+                        this.mGoodShoots.Add(lShoot);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the shoot is too far on x.
+        /// </summary>
+        /// <param name="pShoot"></param>
+        /// <returns></returns>
+        private bool IsTooFarOnX(Shoot pShoot)
+        {
+            return pShoot.CurrentX > this.mTargetArea.MaxX && pShoot.CurrentY >= this.mTargetArea.MinY;
+        }
+
+        /// <summary>
+        /// Returns true if the shoot is too far on Y.
+        /// </summary>
+        /// <param name="pShoot"></param>
+        /// <returns></returns>
+        private bool IsTooFarOnY(Shoot pShoot)
+        {
+            return Math.Abs(pShoot.YSpeedWhenFirstTimeUnder0) > Math.Abs(this.mTargetArea.MinY); 
         }
 
         #endregion
